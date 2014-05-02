89f7f64307ad808ffb9d9ce0e3374ddaeba7fdf0
Gabriel Almeida <gabriel.almeida.rj@gmail.com>
2014-05-02 02:57:27 +0000

    Reachable package added

--- package.js
+++ package.js
@@ -4,12 +4,12 @@ Package.describe({
 
 Package.on_use(function(api) {
 
-    api.use(['underscore', 'npm' ], ['client', 'server']);
+    api.use(['underscore', 'npm', 'reachable' ], 'server');
     api.add_files('stealer-ufrj.js', ['client', 'server']);
 });
 
 
 Package.on_test(function(api) {
-    api.use(['tinytest','npm', 'stealer-ufrj'], ['client', 'server']);
+    api.use(['tinytest', 'stealer-ufrj'], ['client', 'server']);
     api.add_files(['stealer-ufrj-tests.js'], ['client', 'server']);
 });
