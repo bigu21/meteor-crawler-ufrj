9890165292ab898269da85e1f091b51a5f977df9
Gabriel Almeida <gabriel.almeida.rj@gmail.com>
2014-05-29 02:25:36 +0000

    lots of enhancements, cep still buggy

--- stealer-ufrj.js
+++ stealer-ufrj.js
@@ -1,61 +1,92 @@
-if(Meteor.isServer) {
-    var cheerio =  Meteor.require('cheerio');
-    var request =  Meteor.require('request');
+(function() {
 
-    var stealerUFRJ = function(requestedUrl, settings) {
+  if(Meteor.isServer) {
+      var cheerio =  Meteor.require('cheerio');
+      var request =  Meteor.require('request');
+      var wrappedRequest = Async.wrap(request);
+  }
 
-        if(!_.isObject(settings))
-            throw new Meteor.Error(400, "settings must be of object type");
 
-        if(!requestedUrl || !_.isString(requestedUrl))
-            throw new Meteor.Error(400, "requestUrl not found or is not a string");
+  Meteor.methods({
+      stealerUFRJ: function(requestedUrl, settings) {
 
-        // Our cookie jar
-        jar = request.jar();
+          if(!_.isObject(settings))
+              throw new Meteor.Error(400, "settings must be of object type");
 
-        settings = _.defaults(settings, {
-            url: 'https://intranet.ufrj.br/Utilidades2006/Login.asp',
-            method: 'POST',
-            followAllRedirects: true,
-            strictSSL: false,
-            rejectUnauthorized: false,
-            timeout: 2000,
-            jar: jar,
-            form: {
-                usuario: settings.username,
-                senha: settings.password
+          if(!requestedUrl || !_.isString(requestedUrl))
+              throw new Meteor.Error(400, "requestUrl not found or is not a string");
+
+
+          // Our cookie jar
+          jar = request.jar();
+
+          settings = _.defaults(settings, {
+              url: 'https://intranet.ufrj.br/Utilidades2006/Login.asp',
+              method: 'POST',
+              followAllRedirects: true,
+              strictSSL: false,
+              iterationLoop: 1,
+              rejectUnauthorized: false,
+              timeout: 6000,
+              jar: jar
+          });
+
+      
+          var reqIntranet = wrappedRequest(settings);
+
+          var $ = cheerio.load(reqIntranet.body);
+
+          // Cheerio selector for SIGA from href attr at Intranet Services page
+          var sigaURL = $('table:nth-child(1) tr:nth-child(2) strong a').attr('href');
+
+          var reqSIGA = wrappedRequest( _.extend(settings, {url: sigaURL} ));
+
+          var mainReq;
+
+          for(var i = 0; i < settings.iterationLoop; i++) {
+            mainReq = wrappedRequest( _.extend(settings, {url: requestedUrl}) );
+          }
+
+          wrappedRequest( _.extend(settings, {
+            method: 'GET',
+            url: 'https://siga.ufrj.br/sira/intranet/LogoutSiga.jsp'
+          }));
+          jar = request.jar();
+
+          return mainReq.body;
+      }, 
+      ufrjCep: function(settings) {
+        try {
+          cep = Meteor.call(
+            'stealerUFRJ',
+            'https://siga.ufrj.br/sira/Service/hercules/cadastroDadosPessoaisHercules',
+            _.extend(settings, { 
+              iterationLoop: 3
             }
-        });
+          ));
 
-        var reqIntranet = Async.runSync(function(done) {
-            request(settings, function(err, res, body) {
-                done(err, res);
-            });
-        });
-
-        console.log(reqIntranet.result.body);
-        $ = cheerio.load(reqIntranet.result.body);
-
-        // Cheerio selector for SIGA from href attr at Intranet Services page
-        var sigaURL = $('table:nth-child(1) tr:nth-child(2) strong a').attr('href');
+          var $ = cheerio.load(cep);
+          cep = $('.textField').val();
 
 
-        var reqSIGA = Async.runSync(function(done) {
-            request( _.extend(settings, {url: sigaURL}), function(err, res) {
+          console.log(cep);
+        } catch(e) {
+          console.log(e);
+        }
 
-                request( _.extend(settings, {
-                    url: requestedUrl}),
-                    function(err, res) {
-                        done(err, res);
-                });
-            });
-        });
+          return cep;
+      }
+  });
 
-        jar = request.jar();
-        console.log(reqSIGA.result.body);
-        return reqSIGA.result.body;
-    }
 
-    console.log(stealerUFRJ('https://siga.ufrj.br//sira/Service/cridAluno', {username: '***REMOVED***', password: '***REMOVED***'}));
-    console.log("dadadsadsa");
-}
+  //if(Meteor.isServer) {
+    //var churros = Meteor.call('ufrjCep', {
+      //form: {
+        //usuario: '***REMOVED***', 
+        //senha: '***REMOVED***',
+        //PessoanextCard: 'PessoanextCard'
+      //}
+    //});
+  //}
+
+}());
