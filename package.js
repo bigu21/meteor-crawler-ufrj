ad3f28cf96df134a00436f0ec31c923531d20d2d
Gabriel Almeida <gabriel.almeida.rj@gmail.com>
2015-03-23 07:52:48 -0300

    Bump filenames and remove sensitive data

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
-    summary: "Stealer UFRJ crawls data from UFRJ Intranet and SIGA"
+    summary: "Crawl data from UFRJ Intranet and SIGA"
 });
 
 Package.on_use(function(api) {
     api.use(['underscore', 'npm', 'reachable'], 'server');
-    api.add_files('stealer-ufrj.js', ['client', 'server']);
+    api.add_files('crawler-ufrj.js', ['client', 'server']);
 });
 
 
 Package.on_test(function(api) {
-    api.use(['tinytest', 'stealer-ufrj'], ['client', 'server']);
-    api.add_files(['stealer-ufrj-tests.js'], ['client', 'server']);
+    api.use(['tinytest', 'crawler-ufrj'], ['client', 'server']);
+    api.add_files(['crawler-ufrj-tests.js'], ['client', 'server']);
 });
